5064c67bfd7e5e5a75e80ff4fda90dbfa515fc55
DavisCheung <64706459+DavisCheung@users.noreply.github.com>
2022-04-17 19:23:11 -0400

    Cleaned up code
    Ran python black
    Adjusted some comments

--- GrabCutUI.py
+++ GrabCutUI.py
@@ -3,6 +3,49 @@ import cv2 as cv
 
 
 class GrabCutApp:
+    def run(self, img_in, sel_in):
+        # Sets image to the selected file; initializes a blank mask
+        self.img_in = img_in  # Copy of image for selection use
+        self.mask = np.zeros(self.img_in.shape[:2], np.uint8)
+        self.img_out = np.zeros(self.img_in.shape, np.uint8)
+
+        self.ref_img = self.img_in.copy()  # Copy of image for algorithm use
+
+        # Initialize selection
+        self.selection = sel_in
+
+        print(self.selection)
+
+        try:
+            # Internally-used arrays in algorithm
+            bg_model = np.zeros((1, 65), np.float64)
+            fg_model = np.zeros((1, 65), np.float64)
+
+            iter = 5  # Number of algorithm iterations desired
+
+            cv.grabCut(
+                self.ref_img,
+                self.mask,
+                self.selection,
+                bg_model,
+                fg_model,
+                iter,
+                cv.GC_INIT_WITH_RECT,
+            )
+
+        except:
+            import traceback
+
+            traceback.print_exc()
+
+        # Makes a new mask from the grabCut mask
+        # Generates output image via matrix mult. w/ mask
+        self.mask2 = np.where((self.mask == 2) | (self.mask == 0), 0, 1).astype("uint8")
+        self.img_out = self.ref_img * self.mask2[:, :, np.newaxis]
+
+        return self.img_out
+
+    """ Python script version w/ selection code
     GREEN: list[int] = [0, 255, 0]
 
     # Initialization
@@ -40,55 +83,12 @@ class GrabCutApp:
                 abs(self.y_pos - y),
             )
 
-    def run(self, img_in, sel_in):
-        # Sets image to the selected file; initializes a blank mask
-        self.img_in = img_in  # Copy of image for selection use
-        self.mask = np.zeros(self.img_in.shape[:2], np.uint8)
-        self.img_out = np.zeros(self.img_in.shape, np.uint8)
-
-        self.ref_img = self.img_in.copy()  # Copy of image for algorithm use
-
-        # Initialize selection
-        self.selection = sel_in
-
-        print(self.selection)
-
-        try:
-            # Internally-used arrays in algorithm
-            bg_model = np.zeros((1, 65), np.float64)
-            fg_model = np.zeros((1, 65), np.float64)
-
-            iter = 5  # Number of algorithm iterations desired
-
-            cv.grabCut(
-                self.ref_img,
-                self.mask,
-                self.selection,
-                bg_model,
-                fg_model,
-                iter,
-                cv.GC_INIT_WITH_RECT,
-            )
-
-        except:
-            import traceback
-
-            traceback.print_exc()
-
-        # Makes a new mask from the grabCut mask; output image by multiplication
-        self.mask2 = np.where((self.mask == 2) | (self.mask == 0), 0, 1).astype("uint8")
-        self.img_out = self.ref_img * self.mask2[:, :, np.newaxis]
-
-
-        return self.img_out
-
-    """ Old program loop
-    def run(self, img_in):
+    def run(self):
         print("Greetings! Please select an image.")
         print("Note that large images may be hard to edit with this app.\n")
 
         # Old File selection
-        # self.root = tkinter.Tk()
+        self.root = tkinter.Tk()
         self.img_name = filedialog.askopenfilename(
             parent=self.root,
             initialdir="./Test_Images",
--- GrabCutWeb.py
+++ GrabCutWeb.py
@@ -28,6 +28,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 # Converts a file to an OpenCV mat
 def file_to_img(file):
     pil_image = Image.open(file)
@@ -35,10 +36,13 @@ def file_to_img(file):
     img = cv.cvtColor(numpy_image, cv.COLOR_RGB2BGR)
     return img
 
+
 # Clears uploads folder
 def initialize():
     shutil.rmtree("static/uploads/")
     os.makedirs("static/uploads/")
+
+
 initialize()  # Clear previous uploads on launch
 
 # Creates a placeholder image mat given an image file
@@ -46,6 +50,7 @@ def generate_placeholder(file):
     img_mat = file_to_img(file)  # Convert directly to mat to get dimensions
     ph_img = Image.new("RGB", (img_mat.shape[1], img_mat.shape[0]))
     return ph_img
+
 
 # Index page
 @app.route("/", methods=["GET", "POST"])
@@ -67,26 +72,30 @@ def upload_file():
             filename_out = OUTPUT_NAME
             image.save(os.path.join(app.config["UPLOAD_FOLDER"], filename), "png")
             output_img = generate_placeholder(file)  # Placeholder for output image
-            output_img.save(os.path.join(app.config["UPLOAD_FOLDER"], filename_out), "png")
+            output_img.save(
+                os.path.join(app.config["UPLOAD_FOLDER"], filename_out), "png"
+            )
 
             # For OpenCV
             # img_test = file_to_img(file)
-            
-            '''
+
+            """
             retval, buffer = cv.imencode(
                 ".png", GrabCutUI.GrabCutApp().run(img_test)
             )
             response = make_response(buffer.tobytes())
             response.headers["Content-Type"] = "image/png"
-            '''
+            """
             return redirect("/preview")
-            #return render_template("preview.html", imagename=filename, outname=filename_out, imgwidth=img_width, imgheight=img_height)
+            # return render_template("preview.html", imagename=filename, outname=filename_out, imgwidth=img_width, imgheight=img_height)
 
     return render_template("index.html")
+
 
 @app.route("/preview", methods=["GET", "POST"])
 def preview():
     if request.method == "POST":
+        # Receive and convert jQuery values for selection
         x_pos = int(request.form["xPos"])
         y_pos = int(request.form["yPos"])
         w_sel = int(request.form["wSel"])
@@ -108,9 +117,16 @@ def preview():
         img_pv = Image.open(os.path.join(app.config["UPLOAD_FOLDER"], INPUT_NAME))
         img_width = img_pv.width
         img_height = img_pv.height
-        return render_template("preview.html", imagename=INPUT_NAME, outname=OUTPUT_NAME, imgwidth=img_width, imgheight=img_height)
+        return render_template(
+            "preview.html",
+            imagename=INPUT_NAME,
+            outname=OUTPUT_NAME,
+            imgwidth=img_width,
+            imgheight=img_height,
+        )
     else:
         return redirect("/")  # Redirect if user shouldn't be here yet
+
 
 @app.route("/download")
 def download():
