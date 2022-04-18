86d06a9581b326ba57a2791e99c6dfb803a49817
DavisCheung <64706459+DavisCheung@users.noreply.github.com>
2022-04-18 00:56:44 -0400

    Added session functionality

--- GrabCutWeb.py
+++ GrabCutWeb.py
@@ -15,7 +15,9 @@ from flask import (
     redirect,
     send_file,
     url_for,
+    session,
 )
+from flask_session import Session
 from PIL import Image
 
 ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
@@ -24,8 +26,12 @@ INPUT_NAME = "input_image.png"
 OUTPUT_NAME = "output_image.png"
 
 app = Flask(__name__)
+app.secret_key = "1738thek3y"
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 # Check for allowed filenames
 def allowed_file(filename):
@@ -45,11 +51,6 @@ def generate_placeholder(image):
     ph_img = Image.new("RGB", (img_mat.shape[1], img_mat.shape[0]))
     return ph_img
 
-def display_new_page(page, input_data, output_date, width, height):
-    return None
-
-global file_processed   # Really dangerous way of doing it, but for the sake of keeping
-file_processed = False  # everything on one page, it's the only way I see it working
 
 # Index page
 @app.route("/", methods=["GET", "POST"])
@@ -78,8 +79,7 @@ def upload_file():
                 image.save(img_data, "png")
                 output_img.save(ph_data, "png")
 
-                global image_file
-                image_file = image
+                session["input_file"] = image
 
                 img_width = image.width
                 img_height = image.height
@@ -90,76 +90,45 @@ def upload_file():
         
         if int(request.form["xPos"]) > 0:
             # Receive and convert jQuery values for selection
-            global selection
             x_pos = int(request.form["xPos"])
             y_pos = int(request.form["yPos"])
             w_sel = int(request.form["wSel"])
             h_sel = int(request.form["hSel"])
-            selection = (x_pos, y_pos, w_sel, h_sel)
-            '''
-            global file_processed
-            file_processed = True
-            '''
-            # Open input image, convert to array, run it through algorithm
-            img_in = image_file
-            mat_in = cv.cvtColor(np.array(img_in), cv.COLOR_RGB2BGR)
-            mat_out = GrabCutUI.GrabCutApp.run(GrabCutUI.GrabCutApp, mat_in, selection)
-            # Convert output back to PIL Image
-            mat_out = cv.cvtColor(mat_out, cv.COLOR_BGR2RGB)
-            img_out = Image.fromarray(mat_out)
-            out_data = io.BytesIO()
-            img_out.save(out_data, "png")
-            encoded_output = base64.b64encode(out_data.getvalue())
-
-            # Get encoded input
-            in_data = io.BytesIO()
-            img_in.save(in_data, "png")
-            encoded_image = base64.b64encode(in_data.getvalue())
-            img_width = img_in.width
-            img_height = img_in.height
-
-            # For download
-            global dl_copy
-            dl_copy = img_out
-
-            file_processed = False
-            return render_template("preview.html", img_data=encoded_image.decode("utf-8"), out_data=encoded_output.decode("utf-8"), imgwidth=img_width, imgheight=img_height)
-            # return render_template("preview.html")
-        '''
-    elif file_processed:
-        # Open input image, convert to array, run it through algorithm
-        img_in = image_file
-        mat_in = cv.cvtColor(np.array(img_in), cv.COLOR_RGB2BGR)
-        mat_out = GrabCutUI.GrabCutApp.run(GrabCutUI.GrabCutApp, mat_in, selection)
-        # Convert output back to PIL Image
-        mat_out = cv.cvtColor(mat_out, cv.COLOR_BGR2RGB)
-        img_out = Image.fromarray(mat_out)
-        out_data = io.BytesIO()
-        img_out.save(out_data, "png")
-        encoded_output = base64.b64encode(out_data.getvalue())
-
-        # Get encoded input
-        in_data = io.BytesIO()
-        img_in.save(in_data, "png")
-        encoded_image = base64.b64encode(in_data.getvalue())
-        img_width = img_in.width
-        img_height = img_in.height
-
-        # For download
-        global dl_copy
-        dl_copy = img_out
-
-        file_processed = False
-        return render_template("preview.html", img_data=encoded_image.decode("utf-8"), out_data=encoded_output.decode("utf-8"), imgwidth=img_width, imgheight=img_height)
-        '''
+            session["selection"] = (x_pos, y_pos, w_sel, h_sel)
+            return "Got pos values"
     else:
         print("no file")
         return render_template("index.html")
 
+@app.route("/preview")
+def preview():
+    # Open input image, convert to array, run it through algorithm
+    img_in = session["input_file"]
+    mat_in = cv.cvtColor(np.array(img_in), cv.COLOR_RGB2BGR)
+    selection = session["selection"]
+    mat_out = GrabCutUI.GrabCutApp.run(GrabCutUI.GrabCutApp, mat_in, selection)
+    # Convert output back to PIL Image
+    mat_out = cv.cvtColor(mat_out, cv.COLOR_BGR2RGB)
+    img_out = Image.fromarray(mat_out)
+    out_data = io.BytesIO()
+    img_out.save(out_data, "png")
+    encoded_output = base64.b64encode(out_data.getvalue())
+
+    # Get encoded input
+    in_data = io.BytesIO()
+    img_in.save(in_data, "png")
+    encoded_image = base64.b64encode(in_data.getvalue())
+    img_width = img_in.width
+    img_height = img_in.height
+
+    # For download
+    session["output_img"] = img_out
+    return render_template("preview.html", img_data=encoded_image.decode("utf-8"), out_data=encoded_output.decode("utf-8"), imgwidth=img_width, imgheight=img_height)
+
 @app.route("/download")
 def download():
     out_data = io.BytesIO()
-    global dl_copy
+    dl_copy = session["output_img"]
     dl_copy.save(out_data, "png")
     out_data.seek(0)
     return send_file(out_data, mimetype="image/png", as_attachment=True, attachment_filename="output_image")
